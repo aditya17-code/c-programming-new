bedc3889c8e06a7d6378a69509b9bcdf5cb7d0a4
aditya17-code <adityakumardutta1707@gmail.com>
2026-09-25 15:46:39 +0530

    Add files via upload

---
+++ perman.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    float p,r,t,si;
+    printf("enter principal\n");
+    scanf("%f",&p);
+	printf("enter the rate of interest\n");
+	scanf("%f",&r);
+	printf("enter the time period\n");
+	scanf("%f",&t);
+	si=(p*r*t)/100;
+	printf("simple intrest= %f",si);
+	return 0;
+}
+
