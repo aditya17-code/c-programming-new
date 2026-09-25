1e7a43926c325576939d4b588ac710e2fca1ced2
aditya17-code <adityakumardutta1707@gmail.com>
2026-09-25 15:11:04 +0530

    Add files via upload

---
+++ celsius to fah.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+	 int main()
+	 {
+	float c,f ;
+	 printf("enter value in fahrenhit");
+	 scanf("%f",&f);
+	 c=((f-32)*5/9);
+	 printf("converted value is=%.2f",c);
+	 return 0;
+}
